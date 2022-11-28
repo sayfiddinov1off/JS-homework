32c974d1ab3ccc3f63f3f33f010aff52d5c18762
Azamat <105981809+sayfiddinov1off@users.noreply.github.com>
2022-11-28 12:55:52 +0300

    Add files via upload

---
+++ while/main.js
@@ -0,0 +1,40 @@
+// // 1
+// let num = 10;
+// let num1 = 0;
+// while(num1 < num){
+//     num1 += 2;
+//     console.log(num1);
+// }
+
+
+
+// // 2
+// let num = 11;
+// let num1 = 1;
+// while(num1 <= num){
+//     console.log(num1);
+//     num1 += 2;
+// }
+
+
+// xafta
+// let num = +prompt('week');
+// switch (num) {
+//     case 1:
+//         alert('Dushanba');
+//     case 2:
+//         alert('Seshanba');
+//     case 3:
+//         alert('Chorshanba');
+//     case 4:
+//         alert('Payshanba');
+//     case 5:
+//         alert('Juma');
+//     case 6:
+//         alert('Payshanba');
+//     case 7:
+//         alert('Shanba');
+//         break;
+//     default:
+//         alert('boshqa');
+// }
